202acb82eb66a052eaa629a618850b89ec5c57c3
andrei04q <ursan04@gmail.com>
2026-04-26 22:39:24 +0300

    first commit

---
+++ TravelSchedule/Core/ApiParams.swift
@@ -0,0 +1,4 @@
+
+enum ApiParams {
+    static let apiKey = "31e9618d-8341-49ee-8139-d15673b972e8"
+}
---
+++ TravelSchedule/Core/TravelScheduleApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
